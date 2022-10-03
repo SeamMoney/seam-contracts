57620b8c6b15473b46005982b6a383b25ac62685
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-10-02 21:56:07 -0700

    cleaning and add check for cli

--- sidewinder/parseMove.py
+++ sidewinder/parseMove.py
@@ -1,32 +1,49 @@
 # take the directory of the module
 
-from psutil import OSX
 import stransi
 import os
 import sys
 import subprocess
 compile_cmd = "aptos move test  --named-addresses seam=default"
 
+def hasAptosCLI():
+    try:
+        return len(os.system("aptos --version").split(" ")) == 2
+    except:
+        return False
+
+def getCompileCmd(module_name,test=True,named_addresses=["default"]):
+    if test:
+        cmd = "aptos move test "
+    else:
+        cmd = "aptos move compile "
+    cmd+= "--named-addresses " + module_name + "=" + named_addresses[0]
+    return cmd
+
+def compile_move():
+    os.chdir("../sidewinder")
+    os.system(compile_cmd)
+
 def write_sources(move_map):
     os.chdir("../sidewinder")
     try:
-        os.mkdir("move")
+        os.mkdir("sources")
     except:
         pass
 
-    os.chdir("move")
+    os.chdir("sources")
     for file in move_map.keys():
         with open(file, 'w+') as f:
             for line in move_map[file]:
                 f.write(line)
             # file.close()
 
+def copyToml():
+    print(os.getcwd())
+    os.system("cp Move.toml ./sidewinder")
 
-# def parse_info
-
-# navigate to the directory of the module in seamDao
 os.chdir("../SeamDAO")
-
+copyToml()
 
 # res = os.cmd(compile_cmd)
 res = subprocess.getoutput(compile_cmd)
@@ -67,24 +84,18 @@ if res != 0:
                         item_1 = warn_info.split("'")[1]
                         warn_type = "unused_assignment"
                         print("UNUSED assignment found",item_1)
+                    if "Unused parameter" in str(esc):
+                        warn_info = esc
+                        item_1 = warn_info.split("'")[1]
+                        warn_type = "unused_var"
+                        print("UNUSED parameter found",item_1)
                     
-                # print("escapes",escs)
+                    
                 break
-        # var
-
-            
-
-        # print(warn_string)
-        # print(warn_line)
-        # print(warn_file)
-        # print(warn_info)
-        # print("next Line:",warn_lines[2])
-        
         w = {"file":warn_file,"line":warn_line,"string":warn_string,"info":warn_info,"type":warn_type,"item_1":item_1}
         print(w)
         warning_ls.append(w)
         warning_map[(warn_file,warn_line)] = w
-
 
     # open all of the .move files in the directory
     os.chdir("sources")
@@ -109,6 +120,10 @@ if res != 0:
                         if w["type"] == "unused_assignment":
                             index = lines[i-1].find(w["item_1"])
                             lines[i-1] = lines[i-1][:index] + "_" + lines[i-1][index+len(w["item_1"]):]
+                        if w["type"] == "unused_var":
+                            index = lines[i-1].find(w["item_1"])
+                            lines[i-1] = lines[i-1][:index] + "_" + lines[i-1][index+len(w["item_1"]):]
+
 
                         # break
                     i+=1
@@ -117,11 +132,8 @@ if res != 0:
 
     write_sources(move_map)
 
-
-
-# def prefix_param():
-
-# def write_warning_fix():
-
-
-# print(res)
+    # run compile again in sources/sideWinder
+    os.chdir("..")
+    res = subprocess.getoutput(compile_cmd)
+    print(res)
+    
